e54b609588b60294eda0ac36fb8eccf5699cc492
davinderkv1 <davinderkv1@rediffmail.com>
2023-06-01 16:40:14 -0700

    created program to convert units

--- convertprogram.kt
+++ convertprogram.kt
@@ -8,16 +8,55 @@
 
 fun main()
     {
-        println("Program to Convert units")
+        println("******Program to Convert units******\n\n\n")
         print("Enter the value to be converted: ")
-        val value = readLine()!!.toDouble()
+        var value = readLine()!!.toDouble()
         println(value)
         print("Enter the Current unit: ")
-        val currentUnit = readLine()
+        var currentUnit = readLine()
         println(currentUnit)
         print("Enter the unit to which you want to convert value: ")
-        val targetUnit = readline()
+        var targetUnit = readLine()
         println(targetUnit)
-
+        var result : Double = 0.0
+        currentUnit = currentUnit!!.lowercase()
+        targetUnit = targetUnit!!.lowercase()
+        when(currentUnit)
+            {
+                "mi" -> when(targetUnit)
+                    {
+                        "km" -> result = value * 1.60934
+                         "cm" -> result = value * 160934
+                         "in" -> result = value * 63360
+                    }
+                "km" -> when(targetUnit)
+                    {
+                        "in" -> result = value * 39370.1
+                        "mi" -> result = value * 0.621371
+                        "cm" -> result = value * 100000
+                    }
+                "in" -> when (targetUnit) 
+                   {
+                       "km" -> result = value / 39370.1
+                        "mi" -> result = value / 63360
+                        "cm" -> result = value / 0.393701
+                   }
+                "cm" -> when (targetUnit) 
+                   {
+                        "km" -> result = value / 100000
+                        "mi" -> result = value / 160934
+                        "in" -> result =value * 0.393701
+                    }
+                "f" -> when(targetUnit)
+                    {
+                        "c" -> result = ((value-32)*5)/9
+                    }
+                "c" -> when(targetUnit)
+                    {
+                        "f" -> result = (value*9/5)+32
+                    }
+        }
+        println("The converted Value is $result")
+        
 
     }
